7fc1ddaa52dbf69b33b7b8898304a1278d594846
Ross Fletcher <rtbfletch@outlook.com>
2018-02-21 22:55:41 -0800

    add response for no results found and further style

--- js/DoctorSearch-interface.js
+++ js/DoctorSearch-interface.js
@@ -14,31 +14,30 @@ $(document).ready(function() {
     // debugger;
     newSearch.then(function(response) {
       let body = JSON.parse(response);
-      for (let i = 0; i < body.data.length; i++) {
-        $('#result').append(   '<h2>' + body.data[i].profile.first_name +
-                              ' ' + body.data[i].profile.last_name + '</h2>' +
-                              '<img src=' + body.data[i].profile.image_url + '>' +
-                              '<li>' + body.data[i].practices[0].visit_address.street + '</li>' +
-                              '<li>' + body.data[i].practices[0].visit_address.city + ', ' + body.data[i].practices[0].visit_address.state + " " + body.data[i].practices[0].visit_address.zip + '</li>' +
-                              '<li>' + body.data[i].specialties[0].name + '</li>' +
-                              '<li>' + '<b>' + 'Phone # : ' + '</b>' + body.data[i].practices[0].phones[0].number + "</li>"
-        );
+      if ( body.data.length === 0) {
+      $("#result").append("No results found for that search.");
+      } else {
+
+        for (let i = 0; i < body.data.length; i++) {
+          $('#result').append(   '<h2>' + body.data[i].profile.first_name +
+                                ' ' + body.data[i].profile.last_name + '</h2>' +
+                                '<img src=' + body.data[i].profile.image_url + '>' +
+                                '<li>' + body.data[i].practices[0].visit_address.street + '</li>' +
+                                '<li>' + body.data[i].practices[0].visit_address.city + ', ' + body.data[i].practices[0].visit_address.state + " " + body.data[i].practices[0].visit_address.zip + '</li>' +
+                                '<li>' + body.data[i].specialties[0].name + '</li>' +
+                                '<li>' + '<b>' + 'Phone # : ' + '</b>' + body.data[i].practices[0].phones[0].number + "</li>"
+          );
+          if (body.data[i].practices[0].website === undefined) {
+            $('#result').append('<li>' + "No website listed" + '</li>');
+          } else {
+            $('#result').append('<li>' + body.data[i].practices[0].website + '</li>');
+          }
 
 
-        if (body.data[i].practices[0].website === undefined) {
-          $('#result').append('<li>' + "No website listed" + '</li>');
-        } else {
-          $('#result').append('<li>' + body.data[i].practices[0].website + '</li>');
+
         }
-
-
-
-      // if (body.data.length === 0) {
-      //    $('#result').append("<p><em> No results found </em></p>" + "<hr>");
-      //  } else {
-      //      console.log("No results found");
-      //  }
       }
+
     }, function (error) {
       $('.showErrors').text('There was an error: ${error.message}');
 
